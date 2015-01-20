94480c3e42c748b23e07e10e3c5a4e836b800203
doos <doos-doos@mail.ru>
2015-01-20 16:56:15 +0300

    Update NotificationDialog

--- src/edu/passwordStorrager/gui/NotificationDialog.java
+++ src/edu/passwordStorrager/gui/NotificationDialog.java
@@ -70,14 +70,14 @@ public abstract class NotificationDialog extends JDialog {
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                onCancel();
+                onOK();
             }
         });
 
 // call onCancel() on ESCAPE
         contentPane.registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                onCancel();
+                onOK();
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
         pack();
@@ -86,10 +86,5 @@ public abstract class NotificationDialog extends JDialog {
     }
 
     abstract public void onOK();
-
-    private void onCancel() {
-// add your code here if necessary
-        dispose();
-    }
 
 }
