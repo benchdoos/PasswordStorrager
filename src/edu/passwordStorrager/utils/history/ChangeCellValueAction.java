24a932b04ff771c593ad64d9cec5798401aeeec3
doos <doos-doos@mail.ru>
2015-02-19 23:48:43 +0300

    Added ChangeCellValueAction.java

---
+++ src/edu/passwordStorrager/utils/history/ChangeCellValueAction.java
@@ -0,0 +1,32 @@
+package edu.passwordStorrager.utils.history;
+
+import edu.passwordStorrager.gui.MainForm;
+
+import java.awt.*;
+
+public class ChangeCellValueAction implements HistoryAction {
+    private String prevValue;
+    private Point cell;
+    private String value;
+
+    public ChangeCellValueAction(Point p, String prevValue, String newValue) {
+        this.cell = p;
+        this.prevValue = prevValue;
+        this.value = newValue;
+    }
+
+    @Override
+    public String toString() {
+        return "ChangeCellValueAction: {cell[" + cell.x + ";" + cell.y + "] old value: [" + prevValue + "]; new value: [" + value + "]}";
+    }
+
+    @Override
+    public void undo(MainForm mainForm) {
+        mainForm.table.setValueAt(prevValue, cell.x, cell.y);
+    }
+
+    @Override
+    public void redo(MainForm mainForm) {
+        mainForm.table.setValueAt(value, cell.x, cell.y);
+    }
+}
