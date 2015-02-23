bd0b007b7933b186647e04e891d78adae1a22eb3
doos <doos-doos@mail.ru>
2015-02-24 00:09:10 +0300

    Added more constructors

--- src/edu/passwordStorrager/objects/Record.java
+++ src/edu/passwordStorrager/objects/Record.java
@@ -5,6 +5,21 @@ public class Record {
     private String password = "";
     private String site = "";
 
+    public Record() {
+    }
+
+    public Record(String login, String password, String site) {
+        this.login = login;
+        this.password = password;
+        this.site = site;
+    }
+
+    public Record(Object login, Object password, Object site) {
+        this.login = (String) login;
+        this.password = (String) password;
+        this.site = (String) site;
+    }
+
     public String getLogin() {
         return login;
     }
